ff1b7f2cf7de6104da4bdfd76c5e53c63b8602d7
Lucas Ege <lucas@celo.org>
2020-03-04 16:14:35 -0800

    WIP

---
+++ packages/protocol/scripts/python/parseRAUSpreadsheet.py
@@ -0,0 +1,40 @@
+import csv
+import json
+
+HOUR = 60 * 60
+DAY = HOUR * 24
+YEAR = DAY * 365
+MONTH = YEAR / 12
+
+def constructObject(row):
+  grant = {}
+  grant["releaseStartTime"] = row[6]
+  grant["releaseCliffTime"] = int(row[16].replace(',',''))
+  grant["numReleasePeriods"] = 48
+  grant["releasePeriod"] = MONTH
+  grant["amountReleasedPerPeriod"] = row[30]
+  grant["revocable"] = True if row[3] == " Yes " else False
+  grant['beneficiary'] = row[0]
+  grant["releaseOwner"] = "TBD"
+  grant["refundAddress"] = "TBD ASK ASA"
+  grant["subjectToLiquidityProvision"] = True
+  grant["initialDistributionRatio"] = 1000
+  grant["canValidate"] = False
+  grant["canVote"] = True
+  return grant
+
+with open('cLabsRAUs.csv', newline='') as grants:
+  with open('cLabsRAU.json', 'w') as outfile:
+    spamreader = csv.reader(grants, delimiter=',')
+    result = []
+    i = 0
+    for row in spamreader:
+      if i < 14 or i > 62:
+        i += 1
+        continue
+      result.append(constructObject(row))
+      i += 1
+
+    json.dump(result, outfile, indent=2)
+
+  
